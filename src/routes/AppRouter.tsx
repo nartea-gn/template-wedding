5eb369e94fac7089d7dd18432b4ef32bccf3aa43
Nartea GN <nacheva.g96@gmail.com>
2026-06-27 17:53:44 +0200

    Update deploy.yml

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import InvitationPage from '../pages/InvitationPage';
 import RsvpPage from '../pages/RsvpPage';
 import AdminRsvpPage from '../pages/AdminRsvpPage';
 
 export default function AppRouter() {
     return (
-        <BrowserRouter>
+        // 1. Añadimos el basename apuntando a tu repositorio
+        <BrowserRouter basename="/template-wedding">
             <Routes>
                 {/* Ruta principal pública */}
-                <Route path="/" element={<InvitationPage />} />
+                <Route path="/" element={<InvitationPage/>}/>
 
                 {/* Ruta del formulario público */}
-                <Route path="/rsvp" element={<RsvpPage />} />
+                <Route path="rsvp" element={<RsvpPage/>}/>
 
-                {/* Ruta del panel de control de los novios */}
-                <Route path="/admin/rsvp" element={<AdminRsvpPage />} />
+                {/* 2. Quitamos la barra '/' inicial de las subrutas para que sean relativas al basename */}
+                <Route path="admin/rsvp" element={<AdminRsvpPage/>}/>
 
                 {/* Redirección por defecto si escriben cualquier otra ruta */}
-                <Route path="*" element={<Navigate to="/" replace />} />
+                <Route path="*" element={<Navigate to="/" replace/>}/>
             </Routes>
         </BrowserRouter>
     );
